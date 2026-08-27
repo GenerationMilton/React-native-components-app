c7bd117c771fc0a37684bc29884afb283b5c7e61
Milton Munoz <livemilton@gmail.com>
2026-08-26 19:29:30 -0500

    feat((HU-04)-HU(HU-04): :sparkles: components App - React Native - Menu Item

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Href, Link } from 'expo-router'
-import { animationMenuRoutes } from '@/constants/Routes'
+import { animationMenuRoutes, menuRoutes, uiMenuRoutes } from '@/constants/Routes'
 import ThemedView from '@/presentation/shared/ThemedView'
+import MenuItem from '@/presentation/menu/MenuItem'
 
 const ComponentsApp
     = () => {
@@ -11,11 +12,47 @@ const ComponentsApp
 
                 {
                     animationMenuRoutes.map((route, index) => (
-                        <Link href={route.name.split('/')[0] as Href}>
-                            {route.title}
-                        </Link>
+                        <MenuItem
+                            key={route.title}
+                            title={route.title}
+                            icon={route.icon}
+                            name={route.name}
+                            isFirst={index == 0}
+                            isLast={index == animationMenuRoutes.length - 1}
+
+                        />
                     ))
                 }
+
+                <View className='my-3' />
+                {
+                    uiMenuRoutes.map((route, index) => (
+                        <MenuItem
+                            key={route.title}
+                            title={route.title}
+                            icon={route.icon}
+                            name={route.name}
+                            isFirst={index == 0}
+                            isLast={index == animationMenuRoutes.length - 1}
+
+                        />
+                    ))
+                }
+                <View className='my-3' />
+                {
+                    menuRoutes.map((route, index) => (
+                        <MenuItem
+                            key={route.title}
+                            title={route.title}
+                            icon={route.icon}
+                            name={route.name}
+                            isFirst={index == 0}
+                            isLast={index == menuRoutes.length - 1}
+
+                        />
+                    ))
+                }
+
 
 
             </ThemedView>
