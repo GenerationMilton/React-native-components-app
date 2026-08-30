475615d623a43f78460f1b2429c36741ff7b89fa
Milton Munoz <livemilton@gmail.com>
2026-08-30 17:31:41 -0500

    feat((HU-04)-HU(HU-04): :sparkles: components App - React Native - pullToRefresh

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
+import { View, Text, ScrollView, RefreshControl } from 'react-native';
 
 const PullToRefreshScreen = () => {
+
+  const primaryColor = useThemeColor({}, 'primary');
+  const backgoundColor = useThemeColor({
+    dark: 'black', light: 'white'
+  }, 'background');
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  //function to determine refresh or not
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+
+    setTimeout(() => {
+      setIsRefreshing(false)
+    }, 3000);
+  }
+
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          colors={[primaryColor, 'red', 'orange', 'green']}
+          progressBackgroundColor={backgoundColor}
+
+        />
+      }
+
+    >
+      <ThemedView margin>
+        <ThemedText>
+          PullToRefreshScreen
+        </ThemedText>
+      </ThemedView>
+
+    </ScrollView>
+
   );
 };
 export default PullToRefreshScreen;
