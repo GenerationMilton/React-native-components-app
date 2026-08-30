daa08bc2c288be28d7d68f29896102ab60e18edb
Milton Munoz <livemilton@gmail.com>
2026-08-30 16:53:33 -0500

    feat((HU-04)-HU(HU-04): :sparkles: components App - React Native KeyboardAvoidingView

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -3,8 +3,10 @@ import ThemedText from '@/presentation/shared/ThemedText';
 import ThemedView from '@/presentation/shared/ThemedView';
 import ThemeTextInput from '@/presentation/shared/ThemeTextInput';
 import { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
+
+const isIOS = Platform.OS === 'ios';
 const TextInputsScreen = () => {
 
   const [form, setForm] = useState({
@@ -14,23 +16,78 @@ const TextInputsScreen = () => {
   })
 
   return (
-    <ThemedView margin>
+    <KeyboardAvoidingView
+      behavior={isIOS ? 'height' : undefined}
+    >
+      <ScrollView>
+        <ThemedView margin>
 
-      <ThemedCard className='mb-5'>
-        <ThemeTextInput
-          placeholder='Nombre Completo'
-          autoCapitalize={'words'}
-          autoCorrect={false}
-          onChangeText={(text) => setForm({ ...form, name: text })}
-        />
-      </ThemedCard>
+          <ThemedCard className='mb-5'>
+            <ThemeTextInput
+              placeholder='Nombre Completo'
+              autoCapitalize={'words'}
+              autoCorrect={false}
+              onChangeText={(text) => setForm({ ...form, name: text })}
+            />
+            <ThemeTextInput
+              placeholder='Correo electrónico'
+              autoCorrect={false}
+              keyboardType='email-address'
+              onChangeText={(text) => setForm({ ...form, email: text })}
+            />
+            <ThemeTextInput
+              placeholder='Teléfono'
+              autoCorrect={false}
+              keyboardType='phone-pad'
+              onChangeText={(text) => setForm({ ...form, email: text })}
+            />
 
-      <ThemedCard>
-        <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
-      </ThemedCard>
+          </ThemedCard>
+
+          <ThemedCard>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
 
 
-    </ThemedView >
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard
+          //   style={{
+          //   marginBottom: isIOS ? 100 : 10,
+          // }}
+          >
+            <ThemeTextInput
+              placeholder='Teléfono'
+              autoCorrect={false}
+              keyboardType='phone-pad'
+              onChangeText={(text) => setForm({ ...form, email: text })}
+            />
+          </ThemedCard>
+
+
+        </ThemedView >
+        {isIOS && <View style={{ marginBottom: 100 }} />}
+
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default TextInputsScreen;
--- presentation/shared/ThemedCard.tsx
+++ presentation/shared/ThemedCard.tsx
@@ -7,13 +7,13 @@ interface Props extends ViewProps {
 }
 
 
-const ThemedCard = ({ className, children }: Props) => {
+const ThemedCard = ({ className, children, ...rest }: Props) => {
 
 
     return (
         <View
             className={`bg-white dark:bg-black/10 rounded-xl p-2 shadow shadow-black/5 ${className}`}
-
+            {...rest}
         >
             {children}
         </View>
