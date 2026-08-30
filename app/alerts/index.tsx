839ae5188cbbf92d3919c4e9ed6561b16410cd19
Milton Munoz <livemilton@gmail.com>
2026-08-29 23:00:00 -0500

    feat((HU-04)-HU(HU-04): :sparkles: components App - React Native - Alerts

--- app/alerts/index.tsx
+++ app/alerts/index.tsx
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native';
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { View, Text, Alert, SafeAreaView, Button, StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const AlertsScreen = () => {
+
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'destructive',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+
+
   return (
-    <View>
-      <Text>AlertsScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedButton className='mb-5' onPress={createTwoButtonAlert}>2-Button Alert</ThemedButton>
+      <ThemedButton className='mb-5' onPress={createThreeButtonAlert}>3-Button Alert</ThemedButton>
+    </ThemedView>
   );
+
+
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
+
 export default AlertsScreen;
