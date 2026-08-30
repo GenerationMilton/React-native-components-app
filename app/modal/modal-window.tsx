dd9636bba64a8fe89c1d6972dda278ab6a943fb7
Milton Munoz <livemilton@gmail.com>
2026-08-30 18:15:02 -0500

    feat((HU-04)-HU(HU-04): :sparkles: components App - React Native - Modal Window

---
+++ app/modal/modal-window.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ThemedView from '@/presentation/shared/ThemedView'
+import ThemedText from '@/presentation/shared/ThemedText'
+
+const ModalScreen = () => {
+    return (
+        <ThemedView
+            className='justify-center items-center flex-1'
+            bgColor="#A52182"
+        >
+            <ThemedText>Hola, Soy un modal</ThemedText>
+        </ThemedView>
+    )
+}
+
+export default ModalScreen
