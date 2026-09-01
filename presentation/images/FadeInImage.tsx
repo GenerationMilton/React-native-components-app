a5704465b3992b9cf7943ca01f7e59626abd7008
Milton Munoz <livemilton@gmail.com>
2026-08-31 20:50:35 -0500

    feat((HU-04)-HU(HU-04): :sparkles: components App - React Native - InfiniteScroll FadeInImage

---
+++ presentation/images/FadeInImage.tsx
@@ -0,0 +1,44 @@
+import { View, Text, ActivityIndicator, StyleProp, Image, ImageStyle, Animated } from 'react-native'
+import React, { useState } from 'react'
+import { useAnimation } from '@/hooks/useAnimation';
+
+interface Props {
+    uri: string;
+    style: StyleProp<ImageStyle>;
+}
+
+const FadeInImage = ({ uri, style }: Props) => {
+
+    const [isLoading, setIsLoading] = useState(true)
+    const { animatedOpacity, fadeIn } = useAnimation();
+
+
+    return (
+        <View style={{
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            {
+                isLoading && (
+                    <ActivityIndicator
+                        style={{ position: 'absolute' }}
+                        color="grey"
+                        size={30}
+                    />
+                )
+            }
+
+            <ActivityIndicator />
+            <Animated.Image
+                source={{ uri }}
+                style={[style, { opacity: animatedOpacity }]}
+                onLoadEnd={() => {
+                    fadeIn({});
+                    setIsLoading(false);
+                }}
+            />
+        </View>
+    )
+}
+
+export default FadeInImage
